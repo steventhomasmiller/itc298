8d756739106165b3fe9f6088f877c14570fb4201
steventhomasmiller <steventhomasmiller@gmail.com>
2015-04-28 20:19:09 -0700

    Update index.js

--- hapi/index.js
+++ hapi/index.js
@@ -1,21 +1,40 @@
+//pizza.js
+
+var orders = require("./orders");
+
 var hapi = require("hapi");
-
-var server = new hapi.Server(); //initializing server
-
-server.connection({ //configuring the server
-  port: 8000 //listen on this port
+var server = new hapi.Server();
+server.connection({
+  port: 8000
 });
-
-server.start(function() {
-  console.log(server.info);
+server.start();
+server.views({
+  path: "templates",
+  engines: {
+    html: require("handlebars")
+  },
+  isCached: false
 });
 
 server.route({
   method: "GET",
-  path: "/{name}",
-  handler: function(request, reply) {
-    //console.log(request.headers);
-    console.log(request.params);
-    reply("Hello, " + request.params.name + ", from Hapi.js.");
+  path: "/{name?}",
+  handler: function(req, reply){
+    var name = req.params.name || "Anon";
+    reply.view("index.html", {
+      user: name,
+      pizzas: orders.pizzas
+    });
+  }
+});
+
+server.route({
+  method: "POST",
+  path: "/order",
+  handler: function(req, reply) {
+    orders.add(req.payload);
+    reply.view("index.html" , {
+      pizzas: orders.pizzas
+    });
   }
 });
