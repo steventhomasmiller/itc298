025e61cedbc872e4d8d676d04585a3cb9a594d9a
steventhomasmiller <steventhomasmiller@gmail.com>
2015-04-28 20:18:45 -0700

    Update pizza.js

--- hapi/pizza.js
+++ hapi/pizza.js
@@ -1,8 +1,40 @@
 //pizza.js
 
+var orders = require("./orders");
+
 var hapi = require("hapi");
 var server = new hapi.Server();
 server.connection({
-  port: 8000;
+  port: 8000
 });
 server.start();
+server.views({
+  path: "templates",
+  engines: {
+    html: require("handlebars")
+  },
+  isCached: false
+});
+
+server.route({
+  method: "GET",
+  path: "/{name?}",
+  handler: function(req, reply){
+    var name = req.params.name || "Anon";
+    reply.view("index.html", {
+      user: name,
+      pizzas: orders.pizzas
+    });
+  }
+});
+
+server.route({
+  method: "POST",
+  path: "/order",
+  handler: function(req, reply) {
+    orders.add(req.payload);
+    reply.view("index.html" , {
+      pizzas: orders.pizzas
+    });
+  }
+});
